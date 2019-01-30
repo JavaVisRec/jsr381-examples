295d1247c80dc8dd10e869b8e99212e3acd120de
Kevin Berendsen <berendsen.kevin@gmail.com>
2019-01-30 09:58:12 +0100

    Fixed type in data set file.

--- src/main/java/jsr381/example/SimpleLinearRegressionExample.java
+++ src/main/java/jsr381/example/SimpleLinearRegressionExample.java
@@ -6,43 +6,45 @@ import java.io.File;
 import java.io.IOException;
 
 /**
+ * This example uses a Swedish Auto Insurance Dataset to predict the total
+ * payment for all auto insurance claims (in thousands of Swedish Kronor),
+ * given the total number of claims
  *
- * @author zoran
+ * @author Zoran Sevarac
  */
 public class SimpleLinearRegressionExample {
 
     public static void main(String[] args) throws IOException {
-
-        String datasetFile = "SweedenAutoInsurance.csv";
+        // Parameters to load the data set.
+        String datasetFile = "SwedenAutoInsurance.csv";
         int inputsCount = 1;
         int outputsCount = 1;
         String delimiter = ",";
 
-        // create data set from csv file
+        // Create a DataSet object from the local CSV file
         DataSet dataSet = BasicDataSet.fromCSVFile(new File(datasetFile), inputsCount, outputsCount, delimiter);
-        // dta is normalized
 
-        // build model
+
+        // Build the model
         SimpleLinearRegression linReg = DeepNettsSimpleLinearRegression.builder()
-                                                                       .trainingSet(dataSet)
-                                                                       .learningRate(0.1f)
-                                                                       .maxError(0.01f)
-                                                                       .build();
+                .trainingSet(dataSet)
+                .learningRate(0.1f)
+                .maxError(0.01f)
+                .build();
                                                                        // maybe also provide test set and run evaluation after training automatically
 
       //  linReg.train(trainingSet);    // TODO: move training set to train method
         // PerformanceMeasure pm = linReg.test(testSet); // Model intreface with train and test methods
         // System.out.println(pm);
 
+        // Display information about the trained model
         float slope = linReg.getSlope();
         float intercept = linReg.getIntercept();
-
         System.out.println("Trained Model y = " + slope + " * x + " + intercept);
 
-        // use model
+        // Predict the outcome based on some input.
         float someInput = 0.10483871f;
         Float prediction = linReg.predict(someInput);
-
         System.out.println("predicted output for " + (someInput*124) + " is:" + (prediction*422.2));
 
 
