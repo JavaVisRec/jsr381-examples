d6f477de928c610a6f7b1dafa9e53d17dfb49639
Kevin Berendsen <berendsen.kevin@gmail.com>
2019-03-04 17:47:23 +0100

    Removed hard dependency to deepnetts-core-ce. Fixed examples to compile.

--- src/main/java/jsr381/example/Cifar10.java
+++ src/main/java/jsr381/example/Cifar10.java
@@ -1,11 +1,12 @@
 package jsr381.example;
 
+import visrec.ri.ml.classification.DeepNettsImageClassifier;
+
+import javax.visrec.ml.classification.Classifier;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-import javax.visrec.AbstractImageClassifier;
-import visrec.ri.ml.classification.DeepNettsImageClassifier;
 
 /**
  *
@@ -16,22 +17,22 @@ public class Cifar10 {
     public static void main(String[] args) throws IOException {
 
         // BUILD IMAGE CLASSIFIER USING VIS REC API
-        Properties prop = new Properties();
+        Map<String, Object> configuration = new HashMap<>();
         // provide data set properties
-        prop.setProperty("visrec.imageWidth", "32");
-        prop.setProperty("visrec.imageHeight", "32");
-        prop.setProperty("visrec.labelsFile", "/home/zoran/datasets/cifar10/train/labels.txt");   // list of labels in txt file
-        prop.setProperty("visrec.trainingFile", "/home/zoran/datasets/cifar10/train/train.txt");  // list of training images in txt file
-        prop.setProperty("visrec.testFile", "/home/zoran/datasets/cifar10/test/test.txt");        // list of test images in txt file
+        configuration.put("visrec.imageWidth", "32");
+        configuration.put("visrec.imageHeight", "32");
+        configuration.put("visrec.labelsFile", "/home/zoran/datasets/cifar10/train/labels.txt");   // list of labels in txt file
+        configuration.put("visrec.trainingFile", "/home/zoran/datasets/cifar10/train/train.txt");  // list of training images in txt file
+        configuration.put("visrec.testFile", "/home/zoran/datasets/cifar10/test/test.txt");        // list of test images in txt file
 
         // provide model properties -  how to specify network architecture ??? could the model be injected?
         //prop.setProperty("networkArch", "architecture.json"); // how to do it programaticly? for different models?
         // training settings
-        prop.setProperty("visrec.sgd.maxError", "0.03");
-        prop.setProperty("visrec.sgd.learningRate", "0.01");
-        prop.setProperty("modelFile", "cifar10.dnet");  // save trained model in file at the end (model has to provide this feature)
+        configuration.put("visrec.sgd.maxError", "0.03");
+        configuration.put("visrec.sgd.learningRate", "0.01");
+        configuration.put("modelFile", "cifar10.dnet");  // save trained model in file at the end (model has to provide this feature)
 
-        AbstractImageClassifier imageClassifier = DeepNettsImageClassifier.builder().build(prop); // maybe also attach some listener  (or callback function) to be notified when model building is complete?
+        Classifier imageClassifier = DeepNettsImageClassifier.builder().build(configuration); // maybe also attach some listener  (or callback function) to be notified when model building is complete?
         System.out.println("Done building image classifier.");
 
         // TEST VISREC IMAGE CLASSIFIER
--- src/main/java/jsr381/example/MnistDemo.java
+++ src/main/java/jsr381/example/MnistDemo.java
@@ -2,6 +2,7 @@ package jsr381.example;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import javax.visrec.AbstractImageClassifier;
@@ -16,17 +17,17 @@ public class MnistDemo {
 
     public static void main(String[] args) throws IOException {
 
-        Properties props = new Properties();
+        Map<String, Object> configuration = new HashMap<>();
         // provide data set properties
-        props.setProperty(VisRecConstants.IMAGE_WIDTH, "28" );  // width of example images
-        props.setProperty(VisRecConstants.IMAGE_HEIGHT, "28" ); // height of example images
+        configuration.put(VisRecConstants.IMAGE_WIDTH, "28" );  // width of example images
+        configuration.put(VisRecConstants.IMAGE_HEIGHT, "28" ); // height of example images
       //  props.setProperty(VisRecConstants.LABELS_FILE, MnistDemo.class.getResource("../../../../labels.txt").getFile());     // path to filer
-        props.setProperty(VisRecConstants.LABELS_FILE, "D:\\datasets\\mnist\\train\\labels.txt");     // path to file
+        configuration.put(VisRecConstants.LABELS_FILE, "D:\\datasets\\mnist\\train\\labels.txt");     // path to file
         // with labels
         // (maybe
         // this could be also specifid as visrec.labels="label1,label2,label3")
        // props.setProperty(VisRecConstants.TRAINING_FILE, MnistDemo.class.getResource("../../../../train3.txt").getFile());     // file with list of training images (contains image paths and corresponding labels)
-        props.setProperty(VisRecConstants.TRAINING_FILE, "D:\\datasets\\mnist\\train\\train.txt");     // file with list of training images (contains image paths and corresponding labels)
+        configuration.put(VisRecConstants.TRAINING_FILE, "D:\\datasets\\mnist\\train\\train.txt");     // file with list of training images (contains image paths and corresponding labels)
 
         // prop.setProperty("visrec.model", "networkArchitecture.json");    // provide as json object or json file
                        // DeepNetts.VISREC_MODEL
@@ -34,15 +35,15 @@ public class MnistDemo {
         // consider https://www.khronos.org/nnef    and     https://onnx.ai/
         // https://www.khronos.org/registry/NNEF/specs/1.0/nnef-1.0.pdf
        // props.setProperty("visrec.model.deepnetts", MnistDemo.class.getResource("../../../../mnist1.json").getFile()); // Constant should be defined in DeepNetts.VISREC_MODEL
-        props.setProperty("visrec.model.deepnetts", "mnist1.json"); // Constant should be defined in DeepNetts.VISREC_MODEL
+        configuration.put("visrec.model.deepnetts", "mnist1.json"); // Constant should be defined in DeepNetts.VISREC_MODEL
         // or set individual properties but that would be too heavy from here
        // props.setProperty("visrec.model.saveToFile", MnistDemo.class.getResource("../../../../mnist.dnet").getFile());  // save trained model to file at the end
 
         // training settings visrec.deepnetts.optimizationType=adagrad etc.
-        props.setProperty(VisRecConstants.SGD_MAX_ERROR, "0.02" );
-        props.setProperty(VisRecConstants.SGD_LEARNING_RATE, "0.03" );
+        configuration.put(VisRecConstants.SGD_MAX_ERROR, "0.02" );
+        configuration.put(VisRecConstants.SGD_LEARNING_RATE, "0.03" );
 
-        AbstractImageClassifier imageClassifier = DeepNettsImageClassifier.builder().build(props);
+        AbstractImageClassifier imageClassifier = DeepNettsImageClassifier.builder().build(configuration);
         System.out.println("Done building image classifier.");
 
         System.out.println("Testing image classifier...");
