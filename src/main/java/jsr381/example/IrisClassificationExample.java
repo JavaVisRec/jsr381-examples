c059fef9f0f2abfd0b356480f1eb8fa0e57bfd5c
Zoran Sevarac <sevarac@gmail.com>
2019-05-14 12:49:48 +0200

    MnistDemo improved

--- src/main/java/jsr381/example/IrisClassificationExample.java
+++ src/main/java/jsr381/example/IrisClassificationExample.java
@@ -13,20 +13,18 @@ public class IrisClassificationExample {
     public static void main(String[] args) throws IOException {
 
         // load iris data set
-        //DataSet dataSet = BasicDataSet.fromCSVFile("iris_data_normalised.txt", 4, 3, ",");
         DataSet dataSet = DataSetExamples.getIrisClassificationDataSet();
-        DataSet[] trainAndTestSet = DataSets.trainTestSplit(dataSet, 0.7);
+        DataSet[] trainTestSplit = DataSets.trainTestSplit(dataSet, 0.7);
 
         // build multi class classifier using deep netts implementation of feed forward network under the hood
         MultiClassClassifier classifier = DeepNettsMultiClassClassifier.builder()
                                                         .inputsNum(4)
                                                         .hiddenLayers(16)                       // if its a common thing put it in the API
-                                                       // .hiddenActivation(ActivationType.RELU) // how to specify  activation function?  Deep Netts type or VisRec Constant
                                                         .outputsNum(3)                          //I think its ok to use implementation specific type since this is specific impl
                                                         .maxEpochs(10000)
                                                         .maxError(0.05f)
                                                         .learningRate(0.01f)
-                                                        .trainingSet(trainAndTestSet[0])
+                                                        .trainingSet(trainTestSplit[0])
                                                         .build();
 
         // use classifier to predict class
