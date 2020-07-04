8c3aeea5138b880378e0b77beec63f7da96498f5
Nishant Raut <nishant30197@gmail.com>
2020-07-04 15:47:50 +0530

    Adding credit card fraud example
    Signed-off-by: Nishant Raut <nishant30197@gmail.com>

---
+++ src/main/java/jsr381/example/CreditCardFraudDetection.java
@@ -0,0 +1,53 @@
+package jsr381.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.visrec.ml.ClassifierCreationException;
+import javax.visrec.ml.classification.BinaryClassifier;
+import javax.visrec.ml.classification.NeuralNetBinaryClassifier;
+
+import jsr381.example.spam.Classifiable;
+
+/**
+ * @author Nishant
+ *
+ */
+public class CreditCardFraudDetection {
+	public static void main(String[] args) throws IOException, ClassifierCreationException {
+		// create data set from specified file
+		URL fraudCsvResource = CreditCardFraudDetection.class.getClassLoader().getResource("creditcard.csv");
+		if (fraudCsvResource == null) {
+			throw new IOException("creditcard.csv not found");
+		}
+
+		// Build binary classifer based on neural network
+		BinaryClassifier<float[]> fraudClassifier = NeuralNetBinaryClassifier.builder().inputClass(float[].class)
+				.inputsNum(29).hiddenLayers(29, 15).maxError(0.03f).maxEpochs(15000).learningRate(0.001f)
+				.trainingFile(new File(fraudCsvResource.getFile())).build();
+		Float result = fraudClassifier.classify(new CreditCardFraud().getClassifierInput());
+		System.out.println(result);
+	}
+
+	static class CreditCardFraud implements Classifiable<float[], Boolean> {
+
+		private float[] creditCardFraudFeatures;
+		private Boolean isFraud;
+
+		public CreditCardFraud() {
+			creditCardFraudFeatures = new float[29];
+			creditCardFraudFeatures[28] = 0;
+		}
+
+		@Override
+		public float[] getClassifierInput() {
+			return creditCardFraudFeatures;
+		}
+
+		@Override
+		public Boolean getTargetClass() {
+			return isFraud;
+		}
+	}
+}
